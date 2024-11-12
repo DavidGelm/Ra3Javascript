d2d1334f54e96ce31b21dca1f7fa9e832fb50c17
DavidGelm <david.gelma@institutvidreres.cat>
2024-11-12 19:20:12 +0100

    Exercici 12 Javascript

--- RA3.Javascript 2.js
+++ RA3.Javascript 2.js
@@ -1,4 +1,7 @@
-let variable1 = undefined
-let variable2 = null
-let suma = variable1 +variable2
-console.log(suma)
+let boolean  = true;
+let resultat = boolean + ""
+console.log(resultat)
+
+let boolean2 = true;
+let resultat1= String(boolean2)
+console.log(resultat1)
