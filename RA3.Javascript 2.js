95920e49910519620fffc1b69d37cc6197907754
DavidGelm <david.gelma@institutvidreres.cat>
2024-11-12 19:16:50 +0100

    Exercici 11 Javascript

--- RA3.Javascript 2.js
+++ RA3.Javascript 2.js
@@ -1,3 +1,4 @@
-let nom_cognoms = "David Gelmà Corral"
-let resultat = nom_cognoms.slice(0,6) + nom_cognoms.slice(7)
-console.log("Resultat: " + resultat)
+let variable1 = undefined
+let variable2 = null
+let suma = variable1 +variable2
+console.log(suma)
