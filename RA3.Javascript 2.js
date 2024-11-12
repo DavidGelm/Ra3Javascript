fad5d253a1a6ff54332794a6a78b642dcf9a4069
DavidGelm <david.gelma@institutvidreres.cat>
2024-11-12 19:07:13 +0100

    Exercici 8 Javascript

--- RA3.Javascript 2.js
+++ RA3.Javascript 2.js
@@ -1,10 +1,4 @@
-let costat1 = 4
-let costat2 = 4
-
-let costat1_elevat = costat1*costat1
-let costat2_elevat = costat2*costat2
-
-let suma_costats = costat1_elevat + costat2_elevat
-
-let hipotenusa = Math.sqrt(suma_costats)
-console.log(hipotenusa)
+let numero_enter = 8
+let string = "patata"
+let divisio = numero_enter / string
+console.log(divisio)
