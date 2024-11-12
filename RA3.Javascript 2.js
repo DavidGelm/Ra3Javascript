52791e66e55940044298ffce44c9d7250de3b236
DavidGelm <david.gelma@institutvidreres.cat>
2024-11-12 19:14:26 +0100

    Exercici 10 Javascript

--- RA3.Javascript 2.js
+++ RA3.Javascript 2.js
@@ -1,5 +1,3 @@
 let nom_cognoms = "David Gelmà Corral"
-let separar = nom_cognoms.split(" ")
-let primer_cognom = separar[1]
-console.log("Primer cognom: " + primer_cognom)
-console.log("Caracters: " + primer_cognom.length)
+let resultat = nom_cognoms.slice(0,6) + nom_cognoms.slice(7)
+console.log("Resultat: " + resultat)
